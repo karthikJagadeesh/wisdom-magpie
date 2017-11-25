ea5896ec5f6c14a4f17989eb273384c941af1beb
Karthik Jagadeesha <kartu8@gmail.com>
2017-11-25 20:48:24 +0530

    Removed unwanted files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-let Twit = require("twit");
-let twitterKeys = require("./config");
+const Twit = require("twit");
+const twitterKeys = require("./config");
 
-let T = new Twit(twitterKeys);
+const T = new Twit(twitterKeys);
 
-let params = {
-  q: "Modi",
-  count: 10
+// const tweet = {
+//   status:
+//     "Greetings World! I'm a spiritual bot. I retweet tweets from spirtual " +
+//     "masters around the world. #wisdommagpie #spirituality #wisdom"
+// };
+
+// const tweeted = (error, data, response) => {};
+
+// T.post("statuses/update", tweet, tweeted);
+
+const retweet = (name, id) => {
+  const params = {
+    name: name,
+    id: id
+  };
+  const response = _ => {};
+  T.post("statuses/retweet", params, response);
 };
 
-const gotData = (error, data, response) => {
-  console.log(data);
-};
+// let params = {
+//   q: "Narendra Modi",
+//   count: 10
+// };
 
-T.get("search/tweets", params, gotData);
+// const gotData = (error, data, response) => {
+//   let tweets = data.statuses;
+//   tweets.forEach(tweet => {
+//     console.log(tweet.text);
+//   });
+// };
+
+// T.get("search/tweets", params, gotData);
