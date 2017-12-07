4536d68310bc2c240f3cc4932a0bf26d46ec60e0
Karthik Jagadeesha <kartu8@gmail.com>
2017-12-07 23:52:37 +0530

    Delete twitter keys file

--- src/config.js
+++
@@ -1,8 +0,0 @@
-const twitterKeys = {
-  consumer_key: "NwrzXZY8HeXveKBE4dthdbpnB",
-  consumer_secret: "BbtD6xlZW24prqcdxllPBLOcuXY2or47OIqoMOUF66zree6srX",
-  access_token: "934281091485024256-kHquYF60MHbgj6B3yGDBmoqQONdgN2Y",
-  access_token_secret: "16VeZ6IbHAk6hSfGanb4355pMe52m6oPXJCLwy9R6PUxv",
-};
-
-module.exports = twitterKeys;
